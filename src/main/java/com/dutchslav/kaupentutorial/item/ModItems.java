f9b4595085f759c680378c903c74511344e1051a
DutchSlav <wouterrovers@gmail.com>
2022-11-30 08:48:00 +0100

    Added a 8-ball item (With custom class!) Function:  - Gives a random number with a 1s cooldown (EN_US and NL_NL)

--- src/main/java/com/dutchslav/kaupentutorial/item/ModItems.java
+++ src/main/java/com/dutchslav/kaupentutorial/item/ModItems.java
@@ -1,6 +1,8 @@
 package com.dutchslav.kaupentutorial.item;
 
 import com.dutchslav.kaupentutorial.KaupenTutorial;
+import com.dutchslav.kaupentutorial.item.custom.EightBallItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +17,11 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_METHAMPHETAMINE = ITEMS.register("raw_methamphetamine", () -> new Item(
             new Item.Properties().tab(ModCreativeModeTab.DRUG_TAB)));
+
+        public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball", () -> new EightBallItem(
+            new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
